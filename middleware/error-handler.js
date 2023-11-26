675c0a5fabc0ad0da4280646fe8539902e03c96f
Fatemeh Noorali <F.Noorali@Vesalgasht.local>
2023-11-26 10:32:22 +0330

    duplicated mongo errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,18 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
+  let customeError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong. Try again later.",
+  };
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+    return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  if (err.code && err.code === 11000) {
+    customeError.msg = `Duplicated value for ${Object.keys(err.keyValue)}`;
+    customeError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  return res.status(customeError.statusCode).json(customeError);
+};
 
-module.exports = errorHandlerMiddleware
+module.exports = errorHandlerMiddleware;
