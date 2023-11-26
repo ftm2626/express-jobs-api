f9792b42a436c45770552c2333879d348bcb5d2a
Fatemeh Noorali <F.Noorali@Vesalgasht.local>
2023-11-26 09:07:09 +0330

    get all jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,11 +1,19 @@
+const JobSchema = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+
 const getAllJobs = async (req, res) => {
-  res.send("");
+  const jobs = await JobSchema.find({ createdBy: req.user.userId }).sort(
+    "createdAt"
+  );
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 const getJob = async (req, res) => {
   res.send("");
 };
 const createJob = async (req, res) => {
-  res.send("");
+  req.body.createdBy = req.user.userId;
+  const job = await JobSchema.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 const editJob = async (req, res) => {
   res.send("");
