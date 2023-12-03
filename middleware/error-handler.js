362f0cabe90c29467ad01a5ce1d49cc9d7689491
Fatemeh Noorali <F.Noorali@Vesalgasht.local>
2023-12-03 14:15:37 +0330

    validation and no item found error handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -5,8 +5,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong. Try again later.",
   };
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+  if (err.name === "ValidationError") {
+    customeError.msg = Object.values(err.errors).map(item =>item.message).join(',');
+    customeError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  if (err.name === "CastError") {
+    customeError.msg = `No item found with the id : ${err.value}`
+    customeError.statusCode = StatusCodes.NOT_FOUND;
   }
   if (err.code && err.code === 11000) {
     customeError.msg = `Duplicated value for ${Object.keys(err.keyValue)}`;
